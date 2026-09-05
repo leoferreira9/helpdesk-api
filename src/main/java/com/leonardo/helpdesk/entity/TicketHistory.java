58a6931d55af3c2546a336ef1207487c6a625d8c
Leonardo <leobarros_@outlook.com>
2026-09-05 00:35:53 -0300

    feat: add ticket history entity

---
+++ src/main/java/com/leonardo/helpdesk/entity/TicketHistory.java
@@ -0,0 +1,44 @@
+package com.leonardo.helpdesk.entity;
+
+import com.leonardo.helpdesk.enums.TicketAction;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "ticket_history")
+public class TicketHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ticket_id", nullable = false)
+    private Ticket ticket;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(length = 100, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TicketAction action;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    private void prePersist(){
+        this.createdAt = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/leonardo/helpdesk/enums/TicketAction.java
@@ -0,0 +1,10 @@
+package com.leonardo.helpdesk.enums;
+
+public enum TicketAction {
+    TICKET_CREATED,
+    TICKET_ASSIGNED,
+    STATUS_CHANGED,
+    PRIORITY_CHANGED,
+    TICKET_RESOLVED,
+    TICKET_CLOSED
+}
